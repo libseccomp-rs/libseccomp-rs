6017e8345ccc861e1817888df902fb3ee622b09f
Manabu Sugimoto <Manabu.Sugimoto@sony.com>
2021-12-09 23:40:15 +0900

    Add static link check for musl-libc to CI
    Run build/test for statically linking libseccomp with musl-libc.
    In addition, remove composite action and use a script for installing libseccomp.
    
    Signed-off-by: Manabu Sugimoto <Manabu.Sugimoto@sony.com>

---
+++ scripts/install_libseccomp.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: Apache-2.0 or MIT
+#
+# Copyright 2021 Sony Group Corporation
+#
+
+set -o errexit
+
+work_dir="$(mktemp -d --tmpdir build-libseccomp.XXXXX)"
+
+# Variables for libseccomp
+libseccomp_version="2.5.1"
+libseccomp_url="https://github.com/seccomp/libseccomp"
+libseccomp_tarball="libseccomp-${libseccomp_version}.tar.gz"
+libseccomp_tarball_url="${libseccomp_url}/releases/download/v${libseccomp_version}/${libseccomp_tarball}"
+
+# Variables for gperf
+gperf_version="3.1"
+gperf_url="https://ftp.gnu.org/gnu/gperf"
+gperf_tarball="gperf-${gperf_version}.tar.gz"
+gperf_tarball_url="${gperf_url}/${gperf_tarball}"
+
+function finish() {
+    rm -rf "${work_dir}"
+}
+
+trap finish EXIT
+
+function build_and_install_gperf() {
+    echo "Build and install gperf version ${gperf_version}"
+    gperf_install_dir="$(mktemp -d --tmpdir build-gperf.XXXXX)"
+    curl -sLO "${gperf_tarball_url}"
+    tar -xf "${gperf_tarball}"
+    pushd "gperf-${gperf_version}"
+    ./configure --prefix="${gperf_install_dir}"
+    make
+    make install
+    export PATH=$PATH:"${gperf_install_dir}"/bin
+    popd
+    echo "Gperf installed successfully"
+}
+
+function build_and_install_libseccomp() {
+    echo "Build and install libseccomp version ${libseccomp_version}"
+    libseccomp_install_dir=$opt_dir
+    mkdir -p "${libseccomp_install_dir}"
+    curl -sLO "${libseccomp_tarball_url}"
+    tar -xf "${libseccomp_tarball}"
+    pushd "libseccomp-${libseccomp_version}"
+    if [ $opt_musl -eq 1 ]; then
+        # Set FORTIFY_SOURCE=1 because the musl-libc does not have some functions about FORTIFY_SOURCE=2
+        cflags="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1 -O2"
+        ./configure --prefix="${libseccomp_install_dir}" CFLAGS="${cflags}" --enable-static
+    else
+        ./configure --prefix="${libseccomp_install_dir}" --enable-static
+    fi
+    make
+    make install
+    popd
+    echo "Libseccomp installed successfully"
+}
+
+#
+# Print out script usage details
+#
+function usage() {
+cat <<EOF
+Build and install libseccomp library from sources
+
+USAGE:
+  install_libseccomp [-m] [-i DIR]
+
+OPTIONS:
+  -h        : show this help message
+  -m        : install libseccomp library for musl-libc [default: GNU-libc]
+  -i [DIR]  : specify the directory for installing libseccomp library [default: /usr/local]
+EOF
+}
+
+function main() {
+    local opt_musl=0
+    local opt_dir="/usr/local"
+
+    while getopts "hmi:" opt; do
+        case $opt in
+            m)
+                opt_musl=1
+                ;;
+            i)
+                opt_dir="${OPTARG}"
+                ;;
+            h|*)
+                usage
+                exit 1
+                ;;
+        esac
+    done
+
+    echo "${opt_musl}"
+    echo "${opt_dir}"
+
+    pushd "${work_dir}"
+    # gperf is required for building the libseccomp.
+    build_and_install_gperf
+    build_and_install_libseccomp
+    popd
+}
+
+main "$@"
