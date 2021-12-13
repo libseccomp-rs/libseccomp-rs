51b3997950bec3b425742085f66ab747d9052c56
Manabu Sugimoto <Manabu.Sugimoto@sony.com>
2021-12-13 11:57:45 +0900

    ci/scripts: Run build/test against each libseccomp version
    - Do compile and test against the `2.4.3`, `2.5.1`, and `2.5.3`
    that is the latest released version.
    - Modify `install_libseccomp.sh` to be able to install arbitrary
    libseccomp version specified by the option(-v)
    - Create `DEFAULT_LIBSECCOMP_VER` and change from `work_dir` to `WORK_DIR`
    
    Signed-off-by: Manabu Sugimoto <Manabu.Sugimoto@sony.com>

--- scripts/install_libseccomp.sh
+++ scripts/install_libseccomp.sh
@@ -7,27 +7,22 @@
 
 set -o errexit
 
-work_dir="$(mktemp -d --tmpdir build-libseccomp.XXXXX)"
-
-# Variables for libseccomp
-libseccomp_version="2.5.1"
-libseccomp_url="https://github.com/seccomp/libseccomp"
-libseccomp_tarball="libseccomp-${libseccomp_version}.tar.gz"
-libseccomp_tarball_url="${libseccomp_url}/releases/download/v${libseccomp_version}/${libseccomp_tarball}"
-
-# Variables for gperf
-gperf_version="3.1"
-gperf_url="https://ftp.gnu.org/gnu/gperf"
-gperf_tarball="gperf-${gperf_version}.tar.gz"
-gperf_tarball_url="${gperf_url}/${gperf_tarball}"
+# installed libseccomp version by default
+DEFAULT_LIBSECCOMP_VER="2.5.3"
+WORK_DIR="$(mktemp -d --tmpdir build-libseccomp.XXXXX)"
 
 function finish() {
-    rm -rf "${work_dir}"
+    rm -rf "${WORK_DIR}"
 }
 
 trap finish EXIT
 
 function build_and_install_gperf() {
+    gperf_version="3.1"
+    gperf_url="https://ftp.gnu.org/gnu/gperf"
+    gperf_tarball="gperf-${gperf_version}.tar.gz"
+    gperf_tarball_url="${gperf_url}/${gperf_tarball}"
+
     echo "Build and install gperf version ${gperf_version}"
     gperf_install_dir="$(mktemp -d --tmpdir build-gperf.XXXXX)"
     curl -sLO "${gperf_tarball_url}"
@@ -42,6 +37,11 @@ function build_and_install_gperf() {
 }
 
 function build_and_install_libseccomp() {
+    libseccomp_version=${opt_ver}
+    libseccomp_url="https://github.com/seccomp/libseccomp"
+    libseccomp_tarball="libseccomp-${libseccomp_version}.tar.gz"
+    libseccomp_tarball_url="${libseccomp_url}/releases/download/v${libseccomp_version}/${libseccomp_tarball}"
+
     echo "Build and install libseccomp version ${libseccomp_version}"
     libseccomp_install_dir=$opt_dir
     mkdir -p "${libseccomp_install_dir}"
@@ -69,26 +69,31 @@ cat <<EOF
 Build and install libseccomp library from sources
 
 USAGE:
-  install_libseccomp [-m] [-i DIR]
+  install_libseccomp [-m] [-v VERSION] [-i DIR]
 
 OPTIONS:
-  -h        : show this help message
-  -m        : install libseccomp library for musl-libc [default: GNU-libc]
-  -i [DIR]  : specify the directory for installing libseccomp library [default: /usr/local]
+  -h            : show this help message
+  -m            : install libseccomp library for musl-libc [default: GNU-libc]
+  -v [VERSION]  : specify the version of installed libseccomp library [default: ${DEFAULT_LIBSECCOMP_VER}]
+  -i [DIR]      : specify the directory for installing libseccomp library [default: /usr/local]
 EOF
 }
 
 function main() {
+    local opt_ver=${DEFAULT_LIBSECCOMP_VER}
     local opt_musl=0
     local opt_dir="/usr/local"
 
-    while getopts "hmi:" opt; do
+    while getopts "hmi:v:" opt; do
         case $opt in
             m)
                 opt_musl=1
                 ;;
             i)
                 opt_dir="${OPTARG}"
+                ;;
+            v)
+                opt_ver="${OPTARG}"
                 ;;
             h|*)
                 usage
@@ -97,10 +102,7 @@ function main() {
         esac
     done
 
-    echo "${opt_musl}"
-    echo "${opt_dir}"
-
-    pushd "${work_dir}"
+    pushd "${WORK_DIR}"
     # gperf is required for building the libseccomp.
     build_and_install_gperf
     build_and_install_libseccomp
