d62c31692964d8be5743b1b010a7e89f98b418f9
Manabu Sugimoto <Manabu.Sugimoto@sony.com>
2021-12-15 19:27:05 +0900

    Set PKG_CONFIG_PATH when LIBSECCOMP_LIB_PATH is set
    If `LIBSECCOMP_LIB_PATH` is set, pkgconfig should show the version
    of the library in `LIBSECCOMP_LIB_PATH`.
    
    Signed-off-by: Manabu Sugimoto <Manabu.Sugimoto@sony.com>

--- libseccomp/build.rs
+++ libseccomp/build.rs
@@ -3,7 +3,17 @@
 // Copyright 2021 Sony Group Corporation
 //
 
+use std::{env, path};
+
 fn main() {
+    println!("cargo:rerun-if-env-changed=LIBSECCOMP_LIB_PATH");
+
+    if let Ok(path) = env::var("LIBSECCOMP_LIB_PATH") {
+        println!("cargo:rustc-link-search=native={}", path);
+        let pkgconfig = path::Path::new(&path).join("pkgconfig");
+        env::set_var("PKG_CONFIG_PATH", pkgconfig);
+    }
+
     if pkg_config::Config::new()
         .atleast_version("2.5.0")
         .probe("libseccomp")
